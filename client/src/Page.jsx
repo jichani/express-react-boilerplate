606f8b71402f603a44ff849674f2ece7341a4275
jichani <jichan7571@naver.com>
2023-08-28 09:55:33 +0900

    edit

--- client/src/Page.jsx
+++ client/src/Page.jsx
@@ -3,7 +3,7 @@ import Layout from "./components/Layout";
 export default function Page() {
   return (
     <Layout>
-      <div className=" text-red-500">메인페이지</div>
+      <div className=" text-red-500 p-12">메인페이지</div>
     </Layout>
   );
 }
