289d7949e848205a35dd121b7e64918dbb0e6433
jichani <jichan7571@naver.com>
2023-08-28 16:38:51 +0900

    qr 페이지 까지 완료. react-spinner, react-qr-scanner 만듬

--- client/src/app/Qr.jsx
+++ client/src/app/Qr.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import { ClipLoader } from "react-spinners";
 
 export default function Qr() {
+  const [prevResult, setPrevResult] = useState(null);
+  const handleSuccess = (result) => {
+    if (prevResult == null || prevResult !== result) {
+      setPrevResult(result);
+      // mutate 전송 react-query
+    }
+  };
   return (
-    <Layout isFooter>
-      <div>qr</div>
+    <Layout isHeader isFooter>
+      <div className="w-full h-full flex items-center">
+        {/* isLoading = true */}
+        {false ? (
+          <div className="w-full flex justify-center">
+            <ClipLoader />
+          </div>
+        ) : (
+          <QrScanner onDecode={handleSuccess} onError={(error) => console.log(error?.message)} constraints={{ facingMode: "environment" }} scanDelay={500} />
+        )}
+      </div>
     </Layout>
   );
 }
