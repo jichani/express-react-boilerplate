a4406410ad9623881184831a670eec3597bc5ed2
jichani <jichan7571@naver.com>
2023-08-28 14:56:12 +0900

    item 집어넣기 완료

--- client/src/Page.jsx
+++ client/src/Page.jsx
@@ -1,9 +1,36 @@
+import LogoAsset from "./asset/LogoAsset";
 import Layout from "./components/Layout";
+import { tourItems } from "./lib/tourItems";
 
 export default function Page() {
   return (
-    <Layout isHeader isFooter>
-      <div className=" text-red-500">메인페이지</div>
+    <Layout isFooter>
+      <div className=" w-full h-full flex flex-col py-16 px-4 space-y-8">
+        {/* 슬로건 */}
+        <div>
+          <div className="flex items-center">
+            <LogoAsset />
+            <span className=" text-2xl font-semibold">과</span>
+          </div>
+          <p className=" text-2xl font-semibold">함께하는 Deagu 여행</p>
+        </div>
+        {/* 상품리스트 */}
+        <div className="flex flex-col space-y-4">
+          {/* db */}
+          {tourItems.map((item, index) => (
+            <div
+              style={{ backgroundImage: `url(${item.bgImage})` }}
+              className="group transition-all duration-300 cursor-pointer relative bg-cover bg-center w-full h-20 rounded-md border border-gray-300"
+            >
+              <div className="w-full h-full bg-gradient-to-r from-gray-900/50 to-gray-700/50 group-hover:from-gray-900/70 group-hover:to-gray-700/70" />
+              <div className=" absolute inset-0 w-full h-full flex flex-col text-white px-4 justify-center">
+                <h2 className=" group-hover:text-white font-medium text-gray-200 ">{item.title}</h2>
+                <p className=" text-sm text-gray-200 group-hover:text-white">{item.subTitle}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
---
+++ client/src/lib/tourItems.js
@@ -0,0 +1,5 @@
+export const tourItems = [
+  { bgImage: "https://mp-seoul-image-production-s3.mangoplate.com/16122_1420180181211?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80", url: "/tour/1", title: "대구 주변 투어", subTitle: "대구의 숨은 맛집을 찾아라" },
+  { bgImage: "https://blog.kakaocdn.net/dn/eF3ncH/btqvKCQuI3T/fdY82nkckCsm6vJAFS3aNK/img.jpg", url: "/tour/2", title: "대구 주변 놀거리", subTitle: "대구의 놀거리를 즐겨보세요!" },
+  { bgImage: "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/wlQ/image/jIuQ2KNCw-nVBJedc4TShOabXSU.jpg", url: "/tour/3", title: "대구 야경 투어", subTitle: "대구의 아름다운 야경을 경험하세요!" },
+]
