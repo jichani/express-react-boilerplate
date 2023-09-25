99739e574689eee2584f06448b197cf13352bea1
jichani <jichan7571@naver.com>
2023-09-25 16:16:50 +0900

    로그인 구현

--- client/src/components/ProtectedLogin.jsx
+++ client/src/components/ProtectedLogin.jsx
@@ -3,7 +3,7 @@ import useUser from "../hooks/useUser";
 
 export default function ProtectedLogin({ children }) {
   const { isLoggedIn } = useUser();
-  if (!isLoggedIn) {
+  if (isLoggedIn) {
     return <Navigate to="/home" />;
   } else {
     return children;
